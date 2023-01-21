2e05f6fce87cf69dcf2cadda3bcf2cc15e7941e4
attal-kush <38625335+attal-kush@users.noreply.github.com>
2023-01-21 12:37:51 -0500

    Added MultiLabel Exps

---
+++ MedVidCL/models/MultiLabel_Text_Model.py
@@ -0,0 +1,291 @@
+import logging
+import json
+import os
+import sys
+import re
+import unicodedata
+import math
+import argparse
+import random
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import numpy as np
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, f1_score, classification_report, average_precision_score
+import pickle
+
+import torch
+from transformers import (AutoTokenizer, 
+                          AutoModelForSequenceClassification, 
+                          Trainer, 
+                          TrainingArguments,
+                          EarlyStoppingCallback,
+                          EvalPrediction
+                         )
+from datasets import load_metric, Dataset
+
+
+# # Set Env Variables
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--source_dir", type=str, default="../data/text/MedInstr/", help="Directory where source JSON files with text are located")
+parser.add_argument("--early_stopping_patience", type=int, default=3, help="Number of epochs to wait before terminating training if no improvement")
+parser.add_argument("--evaluation_metric", type=str, default="eval_Fweighted", help="Metric to use to track and choose best model")
+parser.add_argument("--seed", type=int, default=42, help="Seed to use to initialize random numbers")
+args = parser.parse_args()
+
+# Set reproducibility
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+random.seed(args.seed)
+os.environ['PYTHONHASHSEED']=str(args.seed)
+
+
+# # Creating One-Hot-Encoded Binary Labels
+
+# In[32]:
+
+
+# Create id's based on new labels
+old_labels_to_new_labels_map = pd.read_csv(args.source_dir + 'level_2_to_level_1_category_label_map.csv')
+new_labels = list(set(old_labels_to_new_labels_map['level_1_category_label'].tolist()))
+old_labels = list(set(old_labels_to_new_labels_map['level_2_category_label'].tolist()))
+id2label = dict(enumerate(new_labels))
+label2id = {v: k for k, v in id2label.items()}
+old_to_new_label_dict = dict(zip(old_labels_to_new_labels_map['level_2_category_label'], old_labels_to_new_labels_map['level_1_category_label']))
+
+def label_vid(queries):
+    # Convert old labels to new labels
+    new_queries = [old_to_new_label_dict[old_label] for old_label in queries]
+    # Create binary labels
+    binary_labels = [1. if id2label[i] in new_queries else 0. for i in range(len(new_labels))]
+    return binary_labels
+
+
+# # Import Datasets
+
+datasets = {}
+
+# Import JSON files first
+json_filenames = [pos_json for pos_json in os.listdir(args.source_dir) if pos_json.endswith('.json')]
+for json_filename in json_filenames:
+    datasets[json_filename] = pd.read_json(args.source_dir + '/' + json_filename)
+    # Rename columns
+    datasets[json_filename] = datasets[json_filename].rename(columns = {'video_title':'title', 'video_id':'YouTube_ID', 'video_sub_title':'text'})
+    # Change labels of new datasets to match the old one
+    datasets[json_filename]['labels'] = datasets[json_filename]['level_2_category_labels'].apply(label_vid)
+    # Remove duplicates
+    datasets[json_filename].drop_duplicates(subset=['YouTube_ID'], inplace=True, ignore_index=True)
+    # Keep only certain columns
+    datasets[json_filename] = datasets[json_filename][["text", "labels", "YouTube_ID"]]
+    
+
+# Inspect Amount of datapoints and unique labels in separate dataframes
+for filename in list(datasets.keys()):
+    print(filename + ": " + str(len(datasets[filename])))
+
+
+# # Train Baseline Transformer Models
+
+def train_and_test_Transformer_Model(model_name = "google/bigbird-roberta-base", max_token_length = 1024, batch_size = 8):
+    # Set variables
+    specific_model = model_name.split('/')[-1]
+    checkpoint_dir = 'checkpoints/' + specific_model + '_runs'
+
+    # Method to Tokenize Text
+    def encode(examples):
+        tokenized_input = tokenizer(
+            examples['text'], 
+            padding = 'max_length', 
+            truncation=True, 
+            max_length = max_token_length)
+
+        return tokenized_input
+    
+    # Get Tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # Create & Tokenize dictionary of pandas datasets converted to Transformer Datasts
+    ddatasets = {}
+    for filename in list(datasets.keys()):
+        ddatasets[filename] = Dataset.from_pandas(datasets[filename])
+        ddatasets[filename] = ddatasets[filename].map(encode, batched=True)
+        ddatasets[filename].set_format('torch')
+        
+    # Initialize Model
+    model = AutoModelForSequenceClassification.from_pretrained(
+                model_name,
+                problem_type="multi_label_classification",
+                num_labels=len(new_labels),
+                id2label=id2label,
+                label2id=label2id)
+    
+    # Define Metrics
+    def multi_label_metrics(predictions, labels):
+        # first, apply sigmoid on predictions which are of shape (batch_size, num_labels)
+        sigmoid = torch.nn.Sigmoid()
+        y_pred = sigmoid(torch.Tensor(predictions))
+        # finally, compute metrics
+        y_true = labels
+        sum_precision = 0
+        number_of_nan_free_precision_scores_for_labels = 0
+        
+        # Find precision score over all nan-free labels and average them
+        for i in range(y_true.shape[1]):
+            precision = average_precision_score(y_true[:, i], y_pred[:, i])
+            if not np.isnan(precision):
+                sum_precision += precision
+                number_of_nan_free_precision_scores_for_labels += 1
+        avgPrecision = sum_precision / number_of_nan_free_precision_scores_for_labels
+        
+        # Find f1 metrics
+        best_threshold = 0
+        best_f1_weighted = 0
+        for threshold in np.arange(0, 0.5, 0.01):
+            preds = np.zeros(y_pred.shape)
+            preds[np.where(y_pred >= threshold)] = 1
+
+            f1_macro = f1_score(y_true, preds, average='macro')
+            f1_micro = f1_score(y_true, preds, average='micro')
+            f1_weighted = f1_score(y_true, preds, average='weighted')
+            
+            if f1_weighted > best_f1_weighted:
+                best_f1_weighted = f1_weighted
+                best_threshold = threshold
+            
+
+        # return as dictionary
+        metrics = {'Avgprecision': avgPrecision,
+                  'F1-Macro': f1_macro,
+                  'F1-Micro': f1_micro,
+                  'Fweighted': best_f1_weighted,
+                  'F1-Threshold': best_threshold}
+        return metrics
+    
+    def compute_metrics(p: EvalPrediction):
+        preds = p.predictions[0] if isinstance(p.predictions, 
+                tuple) else p.predictions
+        result = multi_label_metrics(
+            predictions=preds, 
+            labels=p.label_ids)
+        return result
+    
+    
+    # Set Training Arguments
+    training_args = TrainingArguments(
+        do_train = True,
+        evaluation_strategy = "epoch",
+        load_best_model_at_end=True,
+        logging_strategy = 'epoch',
+        metric_for_best_model = args.evaluation_metric,
+        num_train_epochs = 10,
+        output_dir = checkpoint_dir,
+        overwrite_output_dir = True,
+        per_device_eval_batch_size = batch_size,
+        per_device_train_batch_size = batch_size,
+        remove_unused_columns=True,
+        report_to="none",
+        save_strategy = 'epoch',
+        save_total_limit = 1,
+        seed = args.seed,
+    )
+
+    # Train model
+    trainer = Trainer(
+        model=model, 
+        args=training_args, 
+        tokenizer = tokenizer,
+        train_dataset=ddatasets['train.json'], 
+        eval_dataset=ddatasets['val.json'],
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=args.early_stopping_patience)],
+    )
+
+    trainer.train()
+    
+    # Get Average Precision Score
+    def get_avg_prec_score_and_f1(trainer, test_dataset):
+        probs, label_ids1, *_ = trainer.predict(test_dataset)
+        sigmoid = torch.nn.Sigmoid()
+        probs = sigmoid(torch.Tensor(probs))
+        
+        sum_precision = 0
+        number_of_nan_free_precision_scores_for_labels = 0
+        
+        # Find precision score over all nan-free labels and average them
+        for i in range(label_ids1.shape[1]):
+            precision = average_precision_score(label_ids1[:, i], probs[:, i])
+            if not np.isnan(precision):
+                sum_precision += precision
+                number_of_nan_free_precision_scores_for_labels += 1
+        results = sum_precision / number_of_nan_free_precision_scores_for_labels
+        
+        print("Average Precision Score on Test Set:", results)
+    
+        preds1 = np.zeros(probs.shape)
+        preds1[np.where(probs >= 0.17)] = 1
+        
+        f1_macro = f1_score(label_ids1, preds1, average='macro')
+        f1_micro = f1_score(label_ids1, preds1, average='micro')
+        f1_weighted = f1_score(label_ids1, preds1, average='weighted')
+        
+        print("F1-Score Macro on Test Set: {:.4f}".format(f1_macro))
+        print("F1-Score Micro on Test Set: {:.4f}".format(f1_micro))
+        print("F1-Score Weighted on Test Set: {:.4f}".format(f1_weighted))
+        return (label_ids1, probs, preds1)
+        #return results
+        
+    
+    report_v1 = get_avg_prec_score_and_f1(trainer, ddatasets['test.json'])
+    
+    # Save Model & Tokenizer
+    trainer.save_model('models/best_run_' + specific_model)
+    tokenizer.save_pretrained('models/best_run_' + specific_model)
+    
+    return report_v1
+
+## Train various models
+
+# BigBird-Base
+print("BigBird-Base Train&Testing")
+report = train_and_test_Transformer_Model("google/bigbird-roberta-base", 1024, batch_size = 4)
+
+# Save probs and labels and preds1
+with open('multilabel_text_report_13_labels.pickle', 'wb') as f:
+    pickle.dump(report, f)
+
+
+# ## Check Best Threshold
+
+# Save probs and labels and preds1
+with open('multilabel_text_report_13_labels.pickle', 'rb') as f:
+    report = pickle.load(f)
+
+best_val_threshold = 0.22
+print("Threshold:", best_val_threshold)
+y_pred = np.zeros(report[1].shape)
+y_pred[np.where(report[1] >= best_val_threshold)] = 1
+
+f1_macro = f1_score(report[0], y_pred, average='macro')
+f1_micro = f1_score(report[0], y_pred, average='micro')
+f1_weighted = f1_score(report[0], y_pred, average='weighted')
+
+print("F1-Score Macro on Test Set: {:.4f}".format(f1_macro))
+print("F1-Score Micro on Test Set: {:.4f}".format(f1_micro))
+print("F1-Score Weighted on Test Set: {:.4f}".format(f1_weighted))
+
+
+# ## Check Label-Wise Metrics
+
+y_preds = np.zeros(report[1].shape)
+y_preds[np.where(report[1] >= best_val_threshold)] = 1
+
+class_report = classification_report(y_true = report[0], 
+                                              y_pred = y_preds, 
+                                              target_names = new_labels, 
+                                              digits = 4)
+print(class_report)
+
---
+++ MedVidCL/models/MultiLabel_Video_Model.py
@@ -0,0 +1,336 @@
+import logging
+import json
+import os
+import sys
+import re
+import unicodedata
+import math
+import time
+import copy
+from typing import Tuple
+import random
+import argparse
+import pickle
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import accuracy_score, f1_score, classification_report, average_precision_score
+
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+import torch.nn as nn
+from torchtext.legacy.data import Field, TabularDataset, BucketIterator
+from tensorflow.keras import layers
+from tensorflow import keras
+import tensorflow as tf
+import tensorflow_ranking as tfr
+import keras.backend as K
+import tensorflow_addons as tfa
+
+
+# # Set Env Variables
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--source_dir", type=str, default="../data/text/MedInstr/", help="Directory where source JSON files with text are located")
+parser.add_argument("--i3d_feature_dir", type=str, default="../features/I3D/", help="Directory where I3D-extracted video features are located")
+parser.add_argument("--vit_feature_dir", type=str, default="../features/ViT/", help="Directory where ViT-extracted video features are located")
+parser.add_argument("--seed", type=int, default=42, help="Seed to use to initialize random numbers")
+parser.add_argument("--batch_size", type=int, default=16, help="Batch Size to train the models with")
+parser.add_argument("--epochs", type=int, default=50, help="Number of iterations to spend training the model")
+parser.add_argument("--lstm_learning_rate", type=float, default=4e-4, help="Learning Rate for LSTM Model")
+parser.add_argument("--transformer_learning_rate", type=float, default=5e-5, help="Learning Rate for Transformer Model")
+args = parser.parse_args()
+
+VIT_MAX_SEQ_LENGTH = 20
+VIT_NUM_FEATURES = 768
+
+# Create directory to hold models
+if not os.path.isdir('./multimodal_tmp'):
+    os.mkdir('./multimodal_tmp')
+
+# Set reproducibility
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+random.seed(args.seed)
+os.environ['PYTHONHASHSEED']=str(args.seed)
+tf.random.set_seed(args.seed)
+
+# Set device to GPU if available
+is_cuda = torch.cuda.is_available()
+if is_cuda:
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+
+# # Transformer Video Classifier
+
+class PositionalEmbedding(layers.Layer):
+    '''Create PositionalEmbedding class for Transformer from Keras'''
+    def __init__(self, sequence_length, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.position_embeddings = layers.Embedding(
+            input_dim=sequence_length, output_dim=output_dim
+        )
+        self.sequence_length = sequence_length
+        self.output_dim = output_dim
+
+    def call(self, inputs):
+        # The inputs are of shape: `(batch_size, frames, num_features)`
+        length = tf.shape(inputs)[1]
+        positions = tf.range(start=0, limit=length, delta=1)
+        embedded_positions = self.position_embeddings(positions)
+        return inputs + embedded_positions
+
+    def compute_mask(self, inputs, mask=None):
+        mask = tf.reduce_any(tf.cast(inputs, "bool"), axis=-1)
+        return mask
+
+class TransformerEncoder(layers.Layer):
+    '''Create TransformerEncoder class from Keras'''
+    def __init__(self, embed_dim, dense_dim, num_heads, **kwargs):
+        super().__init__(**kwargs)
+        self.embed_dim = embed_dim
+        self.dense_dim = dense_dim
+        self.num_heads = num_heads
+        
+        # Create standard BERT-Based Transformer model
+        self.attention = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=embed_dim, dropout=0.1
+        )
+        self.dense_proj = keras.Sequential(
+            [layers.Dense(dense_dim, activation=tf.nn.gelu), layers.Dense(embed_dim),]
+        )
+        self.layernorm_1 = layers.LayerNormalization(epsilon=1e-12)
+        self.layernorm_2 = layers.LayerNormalization(epsilon=1e-12)
+
+    def call(self, inputs, mask=None):
+        if mask is not None:
+            mask = mask[:, tf.newaxis, :]
+
+        attention_output = self.attention(inputs, inputs, attention_mask=mask)
+        proj_input = self.layernorm_1(inputs + attention_output)
+        proj_output = self.dense_proj(proj_input)
+        return self.layernorm_2(proj_input + proj_output)
+
+
+# # Creating One-Hot-Encoded Binary Labels
+
+# Create id's based on new labels
+old_labels_to_new_labels_map = pd.read_csv(args.source_dir + 'level_2_to_level_1_category_label_map.csv')
+new_labels = list(set(old_labels_to_new_labels_map['level_1_category_label'].tolist()))
+old_labels = list(set(old_labels_to_new_labels_map['level_2_category_label'].tolist()))
+id2label = dict(enumerate(new_labels))
+label2id = {v: k for k, v in id2label.items()}
+old_to_new_label_dict = dict(zip(old_labels_to_new_labels_map['level_2_category_label'], old_labels_to_new_labels_map['level_1_category_label']))
+
+def label_vid(queries):
+    # Convert old labels to new labels
+    new_queries = [old_to_new_label_dict[old_label] for old_label in queries]
+    # Create binary labels
+    binary_labels = [1. if id2label[i] in new_queries else 0. for i in range(len(new_labels))]
+    return binary_labels
+
+
+# # Method to Import Data of Different Types
+
+def import_datasets():
+    
+    datasets = {}
+    torch_features = {}
+    # Import JSON files first
+    json_filenames = [pos_json for pos_json in os.listdir(args.source_dir) if pos_json.endswith('.json')]
+    video_filenames = [filename.replace(".json", "_vit_v2.json") for filename in json_filenames]
+    for video_filename, json_filename in zip(video_filenames, json_filenames):
+        datasets[json_filename] = pd.read_json(args.source_dir + '/' + json_filename)
+        # Rename columns
+        datasets[json_filename] = datasets[json_filename].rename(columns = {'video_sub_title':'text', 'video_title':'title', 'video_id':'YouTube_ID'})
+        # Change labels of new datasets to match the old one
+        datasets[json_filename]['labels'] = datasets[json_filename]['level_2_category_labels'].apply(label_vid)
+        # Remove duplicates
+        datasets[json_filename].drop_duplicates(subset=['YouTube_ID'], inplace=True, ignore_index=True)
+        # Add feature numpy array
+        datasets[json_filename]['features'] = datasets[json_filename]['YouTube_ID'].apply(lambda x: np.load(args.vit_feature_dir + video_filename[:-5] + '/' + x + '.npy'))
+        # Convert all numpy arrays to float32
+        datasets[json_filename]['features'] = datasets[json_filename]['features'].apply(lambda x: x.astype('float32'))
+        # Convert all numpy arrays to Pytorch tensors
+        datasets[json_filename]['torch_features'] = datasets[json_filename]['features'].apply(lambda x: torch.Tensor(x))
+        # Change each feature column to a list
+        torch_features[json_filename] = torch.nn.utils.rnn.pad_sequence(datasets[json_filename]['torch_features'].to_list(), batch_first=True, padding_value=0)
+    
+    return datasets, torch_features
+
+
+# # Import Datasets
+
+# Import ViT-extracted, 20-frame-based datasets
+vit_datasets, vit_torch_features = import_datasets()
+
+
+# # Train Baseline Transformer Models
+
+# Calculate mean average precision scores
+def mAP(y_true, y_pred):
+    sum_precision = tf.constant(0, dtype=tf.float64)
+    number_of_nan_free_precision_scores_for_labels = tf.constant(0, dtype=tf.float64)
+    
+    for i in range(y_true.shape[1]):
+        precision = tf.py_function(average_precision_score, (y_true[:, i], y_pred[:, i]), tf.double)
+        if not tf.math.is_nan(precision):
+            sum_precision += precision
+            number_of_nan_free_precision_scores_for_labels += tf.constant(1, dtype=tf.float64)
+    return sum_precision / number_of_nan_free_precision_scores_for_labels
+
+
+def f1_weighted_for_py(y_true, y_pred):
+    return f1_score(y_true, y_pred, average='weighted')
+
+
+# Calculate f1-weighted over thresholds
+def f1Weighted(y_true, y_pred):
+    best_threshold = tf.constant(0, dtype=tf.float64)
+    best_f1_weighted = tf.constant(0, dtype=tf.float64)
+    for eval_threshold in np.arange(0.01, 0.5, 0.01):
+        y_pred_numpy = y_pred.numpy()
+        preds = np.zeros(y_pred_numpy.shape)
+        preds[np.where(y_pred_numpy >= eval_threshold)] = 1
+        preds = tf.convert_to_tensor(preds)
+
+        result = tf.py_function(f1_weighted_for_py, (y_true, preds), tf.double)
+        
+        if result > best_f1_weighted:
+            best_f1_weighted = result
+            best_threshold = eval_threshold
+    return best_f1_weighted
+
+
+# Method to train Transformer Model 
+def get_compiled_model(max_seq_length, num_features, learning_rate, dense_dim=12):
+    # Set parameters for Transformer Encoder architecture
+    # Video Parameters
+    sequence_length = max_seq_length
+    embed_dim = num_features
+    dense_dim = dense_dim
+    num_heads = 12
+    
+    classes = len(new_labels)
+    
+    inputs = keras.Input(shape=(None, None))
+    x = PositionalEmbedding(
+        sequence_length, embed_dim, name="frame_position_embedding"
+    )(inputs)
+    x = TransformerEncoder(embed_dim, dense_dim, num_heads, name="transformer_layer")(x)
+    # Add simple Max-pooling layer
+    x = layers.GlobalMaxPooling1D()(x)
+    # Send to Dropout Layer
+    x = layers.Dropout(0.1)(x)
+    # Send to Fully Connected layer to get probabilities for each of the 16 classes
+    outputs = layers.Dense(classes, activation="sigmoid")(x)
+    model = keras.Model(inputs, outputs)
+    
+    # Compile model with Adam optimizer and loss function
+    model.compile(
+        run_eagerly=True, optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_2=0.999), loss="binary_crossentropy", metrics=[mAP,
+                                                                                                                            f1Weighted,
+                                                                                                                            tfa.metrics.F1Score(num_classes=classes,
+                                                                                                                                                average='macro',
+                                                                                                                                                threshold=0.22,
+                                                                                                                                                name = "f1_macro"),
+                                                                                                                            tfa.metrics.F1Score(num_classes=classes,
+                                                                                                                                                average='micro',
+                                                                                                                                                threshold=0.22,
+                                                                                                                                                name = "f1_micro")]
+    )
+    return model
+
+
+def train_and_test_transformer(datasets, labels, filepath, max_seq_length, num_features, learning_rate=1e-5, dense_dim=12, epochs=args.epochs):
+    # Create callbacks
+    checkpoint = keras.callbacks.ModelCheckpoint(
+        filepath, monitor = "val_f1Weighted", mode = "max", save_weights_only=True, save_best_only=True, verbose=1
+    )
+    early_stopping = keras.callbacks.EarlyStopping(monitor='val_f1Weighted', patience=10, mode="max", restore_best_weights=True)
+    
+    # Train model with early stopping based on validation dataset
+    model = get_compiled_model(max_seq_length, num_features, learning_rate, dense_dim)
+    history = model.fit(
+        datasets['train.json'],
+        labels['train.json'],
+        validation_data=(datasets['val.json'], labels['val.json']),
+        epochs=epochs,
+        callbacks=[checkpoint, early_stopping],
+    )
+    
+    # Load in model
+    model.load_weights(filepath)
+    # Test on Set 1
+    test1_preds = model.predict(datasets['test.json'])
+    test1_labels = labels['test.json']
+
+    return (test1_labels, test1_preds)
+
+
+# ## Preprocess all Datasets
+
+vit_transformer_datasets = {}
+vit_transformer_labels = {}
+
+json_filenames = [pos_json for pos_json in os.listdir(args.source_dir) if pos_json.endswith('.json')]
+for json_filename in json_filenames:
+
+    # Develop ViT datasets for Transformers
+    vit_transformer_datasets[json_filename] = vit_torch_features[json_filename].detach().numpy()
+    vit_transformer_labels[json_filename] = np.stack(vit_datasets[json_filename]['labels'].values)
+
+
+# # Train and Test Transformer model on ViT Dataset
+
+report = train_and_test_transformer(vit_transformer_datasets, vit_transformer_labels, "./multimodal_tmp/transformer_vit_video_classifier", VIT_MAX_SEQ_LENGTH, VIT_NUM_FEATURES, args.transformer_learning_rate)
+
+
+# Save report
+with open('multilabel_video_report_13_labels.pickle', 'wb') as f:
+    pickle.dump(report, f)
+
+
+# ## Check Best Threshold
+
+# Load report
+with open('multilabel_video_report_13_labels.pickle', 'rb') as f:
+    report = pickle.load(f)
+
+
+eval_threshold = 0.22
+f1_macro = tfa.metrics.F1Score(num_classes=len(new_labels), threshold=eval_threshold, average="macro")
+f1_macro.update_state(report[0], report[1])
+result = f1_macro.result()
+print("Test Set 1 F1-Macro Score:", result)
+
+f1_micro = tfa.metrics.F1Score(num_classes=len(new_labels), threshold=eval_threshold, average="micro")
+f1_micro.update_state(report[0], report[1])
+result2 = f1_micro.result()
+print("Test Set 1 F1-Micro Score:", result2)
+
+f1_weighted = tfa.metrics.F1Score(num_classes=len(new_labels), threshold=eval_threshold, average="weighted")
+f1_weighted.update_state(report[0], report[1])
+result3 = f1_weighted.result()
+print("Test Set 1 F1-Weighted Score:", result3)
+
+result4 = mAP(report[0], report[1])
+print("Test Set 1 mAP Score:", result4)
+
+
+# ## Check Label-Wise Metrics
+
+y_preds = np.zeros(report[1].shape)
+y_preds[np.where(report[1] >= eval_threshold)] = 1
+
+class_report = classification_report(y_true = report[0], 
+                                              y_pred = y_preds, 
+                                              target_names = new_labels, 
+                                              digits = 4)
+print(class_report)
+
